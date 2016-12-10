34d0f2e76e4f45367aab80465ebef12f20a02bc5
Mika Zakaryan <mikazakaryan94@gmail.com>
2016-12-10 18:01:02 +0400

    Make timer dynamic

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
 #!/usr/bin/env node
 
 /**
- * Module dependencies
- */
+* Module dependencies
+*/
 const horloge = require('commander');
 const chalk = require('chalk');
 
 /**
- * Methods
- */
-const runInterval = function (duration, interval = process.env.HORLOGE_UNIT_VALUE) {
-  if (duration === 0) {
-    console.log("\nTime's up");
-  } else {
-    process.stdout.clearLine();
-    process.stdout.cursorTo(0);
-    process.stdout.write(`${duration}mn left`);
-    // calls runInterval after interval is elapsed with the updated parameters
-    setTimeout(runInterval, interval, duration - 1, interval);
+* Methods
+*/
+
+const changableOutput = (s) => {
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  process.stdout.write(s);
+};
+
+const oneMinute = 60;
+
+const decideToOutput = (duration) => {
+  const secondsOnly = (duration % oneMinute);
+  if (duration <= 0) {
+    console.log('\nTime is up');
+    return 0;
+  } else if (duration % oneMinute === 0) {
+    if (duration / oneMinute <= 5) {
+      if (duration / oneMinute > 0) {
+        changableOutput(`${duration / oneMinute} min`);
+      }
+    }
+  } else if (duration === oneMinute + (oneMinute / 2)) {
+    changableOutput(`${(duration - secondsOnly) / oneMinute} min ${secondsOnly} sec`);
+  } else if (duration === (oneMinute / 2)) {
+    changableOutput(`${duration} sec`);
+  } else if (duration === (oneMinute / 3)) {
+    changableOutput(`${duration} sec`);
+  } else if (duration <= (oneMinute / 6)) {
+    changableOutput(`${duration} sec`);
   }
+}
+
+const runInterval = function (duration, interval = process.env.HORLOGE_UNIT_VALUE) {
+  decideToOutput(duration);
+  // calls runInterval after interval is elapsed with the updated parameters
+  setTimeout(runInterval, interval, duration - 1, interval);
+  return 0;
 };
 
 /**
@@ -32,13 +58,14 @@ const controls = {
       console.log(chalk.red.bold('<<<Please provide an integer value (ex. -> 5, 10, 12)>>>'));
       return;
     }
-    runInterval(duration);
+    runInterval(duration * oneMinute);
+
   },
 };
 
 /**
- * Main
- */
+* Main
+*/
 process.env.HORLOGE_UNIT_VALUE = 1000;
 
 horloge
